3ed8aad2d3f43bfb627038826bdee18edfa4ae39
Amelia Scarbrough <scarbroughm@hotmail.com>
2021-02-04 22:47:30 -0600

    added typing function and some minor styles

--- src/App.js
+++ src/App.js
@@ -14,19 +14,31 @@ const backgrounds = [
 ]
 
 const [background, setBackground] = useState('earth')
+const [scream, setScream] = useState('')
 
+const handleScream = e => {
+  const {value} = e.target
 
+  setScream(value)
+}
 
+// keep as is or make textarea transparent?
   return (
 
     <div className={`${background} container`}>
-
-      {backgrounds.map((bg) => (
-        <button key={bg.bg}  onClick={() => setBackground(bg.bg)}>{bg.bg}</button> 
+      <div className="bg-container">
+        {backgrounds.map((bg) => (
+        <button key={bg.bg}  onMouseOver={() => setBackground(bg.bg)}>{bg.bg}</button> 
         ))} 
+        </div>
+      
+        <textarea type="text" value={scream} onChange={handleScream}/>
+        {/* at the very least could hit enter to submit */}
+        {/* <h1>{scream}</h1> */}
     </div>
    
   );
 }
 
 export default App;
+
