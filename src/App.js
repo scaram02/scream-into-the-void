c97ba9d27bf68f6adbf4767d0eb9a5b8e5604db0
Amelia Scarbrough <scarbroughm@hotmail.com>
2021-02-06 11:05:21 -0600

    message appears

--- src/App.js
+++ src/App.js
@@ -16,19 +16,40 @@ const backgrounds = [
 
 const [background, setBackground] = useState(`earth`) // good
 const [scream, setScream] = useState('')
+// hmmm
+const [message, setMessage] = useState('')
 
 const matchingBg = backgrounds.filter((bg) => bg.bg === background)[0]
 
 
 const handleScream = e => {
+  // set scream
   const {value} = e.target
   setScream(value)
+
+
+  // set validation message
+  const timeout = setTimeout(() => {
+    setMessage('yeah though')
+  }, 1000);
+  
+  return () => {
+        clearTimeout(timeout);
+
+      }
 }
 
-useEffect(() => {
-  const timeout = setTimeout(() => console.log(`change this in a sec`), 1000);
-  return () => clearTimeout(timeout);
-}, [scream]);
+
+// WONDERING if I can reconfigure to make message disappear
+// useEffect(() => {
+//   const timeout = setTimeout(() => console.log(`make the message disappear`), 1000);
+//   return () => {
+//     clearTimeout(timeout);
+//   }
+// }, [message]);
+
+
+
 
 
   return (
@@ -44,11 +65,12 @@ useEffect(() => {
         placeholder="insert scream here"/>
         {/* at the very least could hit enter to submit */}
 
-        <div className="bg-container">
+        <div className="button-container">
         {backgrounds.map((bg) => (
         <button key={bg.bg}  onMouseOver={() => setBackground(`${bg.bg}`)}>{bg.bg}!</button> 
         ))} 
         </div>
+        <h1>{message}</h1>
     </div>
    
   );
