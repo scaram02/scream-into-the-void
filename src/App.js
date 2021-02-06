6ddcb6e2bcf643ae0d089664ce1ad58b40287b15
Amelia Scarbrough <scarbroughm@hotmail.com>
2021-02-06 11:59:27 -0600

    random messages, now disappear

--- src/App.js
+++ src/App.js
@@ -10,60 +10,65 @@ const backgrounds = [
  {bg: "cliff", color: 'darkgreen'}, 
  {bg: "well", color: "red"},
  {bg: "canyon", color: 'pink'}, 
-//  {bg: "lake", color: 'gray'}
 ]
+
+const messages = [
+  "Dude yeah, I feel that", 
+  "Let it out! That's valid", 
+  "That was a good scream, 10/10", 
+  "You're right about that one",
+  "Rough day at the office?",
+  "That's right. Let it out"
+]
+
 
 
 const [background, setBackground] = useState(`earth`) // good
 const [scream, setScream] = useState('')
-// hmmm
 const [message, setMessage] = useState('')
 
 const matchingBg = backgrounds.filter((bg) => bg.bg === background)[0]
 
+const randomMessage = messages[Math.floor(Math.random() * messages.length)]
+const showMessage = scream.toLowerCase().includes("aaa")? `${scream.split(' ')[0]} is right` : randomMessage
 
 const handleScream = e => {
   // set scream
   const {value} = e.target
   setScream(value)
-
-
   // set validation message
   const timeout = setTimeout(() => {
-    setMessage('yeah though')
+    setMessage(showMessage)
   }, 1000);
-  
   return () => {
         clearTimeout(timeout);
-
       }
 }
 
 
-// WONDERING if I can reconfigure to make message disappear
-// useEffect(() => {
-//   const timeout = setTimeout(() => console.log(`make the message disappear`), 1000);
-//   return () => {
-//     clearTimeout(timeout);
-//   }
-// }, [message]);
+// make the message disappear
+useEffect(() => {
+  const timeout = setTimeout(() => setMessage(''), 1000);
+  return () => {
+    clearTimeout(timeout);
+  }
+}, [message]);
 
 
+// the problem is they allload 2 seconds after but if you type multiple letters a second then that doesn't matter, it's still 2 seconds later but shows a message EVERY time you type, not just2 scs after pause. need to see user has STOPPED typing rather than typed, and/or JSUT render the last one
 
-
+// but actually what I really want is to have the message replace the text.... soooo
 
   return (
 
     <div className={`${background} container`}>
-      
-      
+  
         <textarea style={{color: matchingBg.color}} 
         spellCheck="false" 
         type="text" 
         value={scream} 
         onChange={handleScream} 
         placeholder="insert scream here"/>
-        {/* at the very least could hit enter to submit */}
 
         <div className="button-container">
         {backgrounds.map((bg) => (
